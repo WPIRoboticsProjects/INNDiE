7a83249566206118155432aebf491d2fe6b1930d
Ryan Benasutti <firey45@gmail.com>
2019-08-28 16:14:02 -0400

    Resolve TODO in Code

--- core/core/src/main/kotlin/edu/wpi/axon/core/dsl/Code.kt
+++ core/core/src/main/kotlin/edu/wpi/axon/core/dsl/Code.kt
@@ -26,8 +26,6 @@ interface Code<out T : Code<T>> {
     /**
      * The [Code] this [Code] depends on. The code generation for these will happen before the
      * code generation for this [Code].
-     *
-     * TODO: Test this for dependencies of both variables and other tasks
      */
     val dependencies: Set<T>
 
