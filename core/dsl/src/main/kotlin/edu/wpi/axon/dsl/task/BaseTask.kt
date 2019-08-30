5dafa5d11064126bdb4d2b048994ba71023ce5fd
Ryan Benasutti <firey45@gmail.com>
2019-08-30 18:46:19 -0400

    Add tests for import validation in BaseTask

--- core/dsl/src/main/kotlin/edu/wpi/axon/dsl/task/BaseTask.kt
+++ core/dsl/src/main/kotlin/edu/wpi/axon/dsl/task/BaseTask.kt
@@ -1,12 +1,18 @@
 package edu.wpi.axon.dsl.task
 
+import arrow.data.Valid
+import edu.wpi.axon.dsl.imports.ImportValidator
+import org.koin.core.KoinComponent
+import org.koin.core.inject
+
 /**
  * Holds the common implementation details for Tasks.
  */
-abstract class BaseTask(override val name: String) : Task {
+abstract class BaseTask(override val name: String) : Task, KoinComponent {
 
-    // TODO: Add import validation
-    override fun isConfiguredCorrectly() =
+    private val importValidator: ImportValidator by inject()
+
+    override fun isConfiguredCorrectly() = importValidator.validateImports(imports) is Valid &&
         inputs.all { it.isConfiguredCorrectly() } && outputs.all { it.isConfiguredCorrectly() }
 
     override fun toString() = "Task(name='$name', class='${this::class.simpleName}')"
---
+++ core/dsl/src/test/kotlin/edu/wpi/axon/dsl/task/BaseTaskTest.kt
@@ -0,0 +1,90 @@
+package edu.wpi.axon.dsl.task
+
+import arrow.data.Invalid
+import arrow.data.Nel
+import arrow.data.Valid
+import com.natpryce.hamkrest.assertion.assertThat
+import edu.wpi.axon.dsl.Code
+import edu.wpi.axon.dsl.configuredIncorrectly
+import edu.wpi.axon.dsl.imports.Import
+import edu.wpi.axon.dsl.imports.ImportValidator
+import edu.wpi.axon.dsl.variable.Variable
+import edu.wpi.axon.testutil.isFalse
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.module
+import org.koin.test.KoinTest
+
+internal class BaseTaskTest : KoinTest {
+
+    @AfterEach
+    fun afterEach() {
+        stopKoin()
+    }
+
+    @Test
+    fun `an invalid input means the task is invalid`() {
+        startKoin {
+            modules(module {
+                single<ImportValidator> {
+                    mockk { every { validateImports(any()) } returns Valid(emptySet()) }
+                }
+            })
+        }
+
+        val task = stubTask(inputs = setOf(configuredIncorrectly()))
+        assertThat(task.isConfiguredCorrectly(), isFalse())
+    }
+
+    @Test
+    fun `an invalid output means the task is invalid`() {
+        startKoin {
+            modules(module {
+                single<ImportValidator> {
+                    mockk { every { validateImports(any()) } returns Valid(emptySet()) }
+                }
+            })
+        }
+
+        val task = stubTask(outputs = setOf(configuredIncorrectly()))
+        assertThat(task.isConfiguredCorrectly(), isFalse())
+    }
+
+    @Test
+    fun `an invalid import means the task is invalid`() {
+        val imports = setOf(Import.ModuleOnly(""))
+
+        val mockImportValidator = mockk<ImportValidator> {
+            every { validateImports(imports) } returns
+                Invalid(Nel.fromListUnsafe(imports.toList()))
+        }
+
+        startKoin {
+            modules(module {
+                single { mockImportValidator }
+            })
+        }
+
+        val task = stubTask(imports = imports)
+        assertThat(task.isConfiguredCorrectly(), isFalse())
+    }
+
+    private fun stubTask(
+        name: String = "task1",
+        imports: Set<Import> = setOf(),
+        inputs: Set<Variable> = setOf(),
+        outputs: Set<Variable> = setOf(),
+        dependencies: Set<Code<*>> = setOf(),
+        code: String = ""
+    ): Task = object : BaseTask(name) {
+        override val imports: Set<Import> = imports
+        override val inputs: Set<Variable> = inputs
+        override val outputs: Set<Variable> = outputs
+        override val dependencies: Set<Code<*>> = dependencies
+        override fun code() = code
+    }
+}
