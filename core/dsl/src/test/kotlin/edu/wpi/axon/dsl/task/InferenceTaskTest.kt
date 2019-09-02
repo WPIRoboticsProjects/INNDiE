dae6fa1d07d2823436453a48b82c6a3efe2d8526
Ryan Benasutti <firey45@gmail.com>
2019-09-01 23:36:05 -0400

    Remove redundant tests from InferenceTaskTest

--- core/dsl/src/test/kotlin/edu/wpi/axon/dsl/task/InferenceTaskTest.kt
+++ core/dsl/src/test/kotlin/edu/wpi/axon/dsl/task/InferenceTaskTest.kt
@@ -1,120 +1,16 @@
 package edu.wpi.axon.dsl.task
 
-import com.natpryce.hamkrest.assertion.assertThat
 import edu.wpi.axon.dsl.alwaysValidImportValidator
 import edu.wpi.axon.dsl.configuredCorrectly
-import edu.wpi.axon.dsl.configuredIncorrectly
 import edu.wpi.axon.testutil.KoinTestFixture
-import edu.wpi.axon.testutil.isFalse
-import edu.wpi.axon.testutil.isTrue
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
 
 internal class InferenceTaskTest : KoinTestFixture() {
 
     private val inferenceTaskName = "task"
-
-    @Test
-    fun `input cannot be uninitialized`() {
-        startKoin {
-            modules(module {
-                alwaysValidImportValidator()
-            })
-        }
-
-        val task = InferenceTask(inferenceTaskName).apply {
-            inferenceSession = configuredCorrectly()
-            output = configuredCorrectly()
-        }
-
-        assertThrows<UninitializedPropertyAccessException> { task.isConfiguredCorrectly() }
-    }
-
-    @Test
-    fun `inferenceSession cannot be uninitialized`() {
-        startKoin {
-            modules(module {
-                alwaysValidImportValidator()
-            })
-        }
-
-        val task = InferenceTask(inferenceTaskName).apply {
-            input = configuredCorrectly()
-            output = configuredCorrectly()
-        }
-
-        assertThrows<UninitializedPropertyAccessException> { task.isConfiguredCorrectly() }
-    }
-
-    @Test
-    fun `output cannot be uninitialized`() {
-        startKoin {
-            modules(module {
-                alwaysValidImportValidator()
-            })
-        }
-
-        val task = InferenceTask(inferenceTaskName).apply {
-            input = configuredCorrectly()
-            inferenceSession = configuredCorrectly()
-        }
-
-        assertThrows<UninitializedPropertyAccessException> { task.isConfiguredCorrectly() }
-    }
-
-    @Test
-    fun `all inputs must be configured correctly`() {
-        startKoin {
-            modules(module {
-                alwaysValidImportValidator()
-            })
-        }
-
-        val task = InferenceTask(inferenceTaskName).apply {
-            input = configuredCorrectly()
-            inferenceSession = configuredIncorrectly()
-            output = configuredCorrectly()
-        }
-
-        assertThat(task.isConfiguredCorrectly(), isFalse())
-    }
-
-    @Test
-    fun `all outputs must be configured correctly`() {
-        startKoin {
-            modules(module {
-                alwaysValidImportValidator()
-            })
-        }
-
-        val task = InferenceTask(inferenceTaskName).apply {
-            input = configuredCorrectly()
-            inferenceSession = configuredCorrectly()
-            output = configuredIncorrectly()
-        }
-
-        assertThat(task.isConfiguredCorrectly(), isFalse())
-    }
-
-    @Test
-    fun `configured correctly when all parameters are non-null and all inputs and outputs are configured correctly`() {
-        startKoin {
-            modules(module {
-                alwaysValidImportValidator()
-            })
-        }
-
-        val task = InferenceTask(inferenceTaskName).apply {
-            input = configuredCorrectly()
-            inferenceSession = configuredCorrectly()
-            output = configuredCorrectly()
-        }
-
-        assertThat(task.isConfiguredCorrectly(), isTrue())
-    }
 
     @Test
     fun `test code`() {
