31927b18ca7f9642d941c965f402715e2478ae9a
Austin Shalit <austinshalit@gmail.com>
2019-09-14 09:03:56 -0700

    Add launcher

--- ui/ui.gradle.kts
+++ ui/ui.gradle.kts
@@ -1,6 +1,25 @@
 plugins {
-    id("org.gretty") version "2.3.1"
+    application
+    //id("org.gretty") version "2.3.1"
     id("com.devsoap.vaadin-flow") version "1.2"
+}
+
+dependencies {
+    fun jetty(group: String = "org.eclipse.jetty", name: String, version: String = "9.4.20.v20190813") =
+            create(group = group, name = name, version = version)
+    implementation(jetty(name = "jetty-server"))
+    implementation(jetty(name = "jetty-webapp"))
+    implementation(jetty(group = "org.eclipse.jetty.websocket", name = "websocket-server"))
+}
+
+application {
+    applicationName = "Axon"
+    mainClassName = "edu.wpi.axon.ui.LauncherKt"
+}
+
+node {
+    download = true
+    nodeModulesDir = file("src/main/electron")
 }
 
 vaadin {
