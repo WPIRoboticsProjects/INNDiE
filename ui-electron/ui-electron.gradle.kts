ee6c793573234d834ee65f292322b94a5bebed02
Austin Shalit <austinshalit@gmail.com>
2019-09-19 22:44:50 -0400

    Allow building of application bundles

--- ui-electron/ui-electron.gradle.kts
+++ ui-electron/ui-electron.gradle.kts
@@ -8,18 +8,38 @@ node {
     download = true
 }
 
+val vaadinDir = projectDir.resolve("vaadin")
+
 tasks {
+    val cleanVaadinResources by registering(Delete::class) {
+        group = "electron"
+
+        delete(vaadinDir)
+    }
     val syncVaadinResources by registering(Sync::class) {
         group = "electron"
         dependsOn(":ui-vaadin:buildProduct")
 
         from(projectDir.resolve("../ui-vaadin/build/output/ui-vaadin"))
-        into(projectDir.resolve("vaadin"))
+        into(vaadinDir)
     }
     val run by registering(NpmTask::class) {
         group = "electron"
         dependsOn("npmInstall", syncVaadinResources)
 
         setArgs(listOf("start"))
+    }
+    val bundle by registering(NpmTask::class) {
+        group = "electron"
+        dependsOn("npmInstall", syncVaadinResources)
+
+        setArgs(listOf("run-script", "bundle"))
+    }
+
+    clean {
+        dependsOn(cleanVaadinResources)
+    }
+    assemble {
+        dependsOn(syncVaadinResources)
     }
 }
