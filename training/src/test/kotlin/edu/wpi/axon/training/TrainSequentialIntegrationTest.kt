f41aea29daa5ef65db4c2162fc19c8c036d1539b
Octogonapus <firey45@gmail.com>
2019-09-29 13:00:35 -0400

    Add Train class (#86)
    * Add Train class
    
    * Add names to call arguments

--- training/src/test/kotlin/edu/wpi/axon/training/TrainSequentialIntegrationTest.kt
+++ training/src/test/kotlin/edu/wpi/axon/training/TrainSequentialIntegrationTest.kt
@@ -1,4 +1,5 @@
 @file:SuppressWarnings("LongMethod", "LargeClass")
+
 package edu.wpi.axon.training
 
 import edu.wpi.axon.dsl.defaultModule
@@ -8,16 +9,12 @@ import edu.wpi.axon.tfdata.Model
 import edu.wpi.axon.tfdata.layer.trainable
 import edu.wpi.axon.tfdata.loss.Loss
 import edu.wpi.axon.tfdata.optimizer.Optimizer
-import edu.wpi.axon.tflayerloader.DefaultLayersToGraph
-import edu.wpi.axon.tflayerloader.LoadLayersFromHDF5
-import io.kotlintest.assertions.arrow.either.shouldBeRight
 import io.kotlintest.assertions.arrow.validation.shouldBeInvalid
 import io.kotlintest.assertions.arrow.validation.shouldBeValid
 import io.kotlintest.matchers.types.shouldBeInstanceOf
 import io.kotlintest.shouldBe
 import org.junit.jupiter.api.Test
 import org.koin.core.context.startKoin
-import java.io.File
 
 internal class TrainSequentialIntegrationTest : KoinTestFixture() {
 
@@ -27,9 +24,7 @@ internal class TrainSequentialIntegrationTest : KoinTestFixture() {
             modules(defaultModule())
         }
 
-        val localModelPath = TrainSequentialIntegrationTest::class.java
-            .getResource("badModel1.h5").toURI().path
-
+        val localModelPath = this::class.java.getResource("badModel1.h5").toURI().path
         TrainSequential(
             userModelPath = localModelPath,
             userDataset = Dataset.Mnist,
@@ -48,94 +43,89 @@ internal class TrainSequentialIntegrationTest : KoinTestFixture() {
         }
 
         val modelName = "custom_fashion_mnist.h5"
-        val localModelPath = TrainSequentialIntegrationTest::class.java
-            .getResource(modelName).toURI().path
-        val layers = LoadLayersFromHDF5(DefaultLayersToGraph()).load(File(localModelPath))
-
-        layers.attempt().unsafeRunSync().shouldBeRight { model ->
-            model.shouldBeInstanceOf<Model.Sequential> {
-                TrainSequential(
-                    userModelPath = localModelPath,
-                    userDataset = Dataset.Mnist,
-                    userOptimizer = Optimizer.Adam(0.001, 0.9, 0.999, 1e-7, false),
-                    userLoss = Loss.SparseCategoricalCrossentropy,
-                    userMetrics = setOf("accuracy"),
-                    userEpochs = 50,
-                    userNewLayers = it.layers.mapIndexedTo(mutableSetOf()) { index, layer ->
-                        // Only train the last 3 layers
-                        if (it.layers.size - index <= 3) layer.layer.trainable()
-                        else layer.layer.trainable(false)
-                    }
-                ).generateScript().shouldBeValid {
-                    it.a shouldBe """
-                    |import tensorflow as tf
-                    |
-                    |model = tf.keras.models.load_model("$modelName")
-                    |
-                    |newModel = tf.keras.Sequential([
-                    |    model.get_layer("conv2d_6"),
-                    |    model.get_layer("conv2d_7"),
-                    |    model.get_layer("max_pooling2d_3"),
-                    |    model.get_layer("dropout_6"),
-                    |    model.get_layer("flatten_3"),
-                    |    model.get_layer("dense_6"),
-                    |    model.get_layer("dropout_7"),
-                    |    model.get_layer("dense_7")
-                    |])
-                    |newModel.get_layer("conv2d_6").trainable = False
-                    |newModel.get_layer("conv2d_7").trainable = False
-                    |newModel.get_layer("max_pooling2d_3").trainable = False
-                    |newModel.get_layer("dropout_6").trainable = False
-                    |newModel.get_layer("flatten_3").trainable = False
-                    |newModel.get_layer("dense_6").trainable = True
-                    |newModel.get_layer("dropout_7").trainable = True
-                    |newModel.get_layer("dense_7").trainable = True
-                    |
-                    |checkpointCallback = tf.keras.callbacks.ModelCheckpoint(
-                    |    "sequential_3-weights.{epoch:02d}-{val_loss:.2f}.hdf5",
-                    |    monitor="val_loss",
-                    |    verbose=1,
-                    |    save_best_only=False,
-                    |    save_weights_only=True,
-                    |    mode="auto",
-                    |    save_freq="epoch",
-                    |    load_weights_on_restart=False
-                    |)
-                    |
-                    |newModel.compile(
-                    |    optimizer=tf.keras.optimizers.Adam(0.001, 0.9, 0.999, 1.0E-7, False),
-                    |    loss=tf.keras.losses.sparse_categorical_crossentropy,
-                    |    metrics=["accuracy"]
-                    |)
-                    |
-                    |earlyStoppingCallback = tf.keras.callbacks.EarlyStopping(
-                    |    monitor="val_loss",
-                    |    min_delta=0,
-                    |    patience=10,
-                    |    verbose=1,
-                    |    mode="auto",
-                    |    baseline=None,
-                    |    restore_best_weights=False
-                    |)
-                    |
-                    |(xTrain, yTrain), (xTest, yTest) = tf.keras.datasets.mnist.load_data()
-                    |
-                    |scaledXTest = xTest.reshape(-1, 28, 28, 1) / 255
-                    |
-                    |scaledXTrain = xTrain.reshape(-1, 28, 28, 1) / 255
-                    |
-                    |newModel.fit(
-                    |    scaledXTrain,
-                    |    yTrain,
-                    |    batch_size=None,
-                    |    epochs=50,
-                    |    verbose=2,
-                    |    callbacks=[checkpointCallback, earlyStoppingCallback],
-                    |    validation_data=(scaledXTest, yTest),
-                    |    shuffle=True
-                    |)
-                    """.trimMargin()
+        val (model, path) = loadModel(modelName)
+        model.shouldBeInstanceOf<Model.Sequential> {
+            TrainSequential(
+                userModelPath = path,
+                userDataset = Dataset.Mnist,
+                userOptimizer = Optimizer.Adam(0.001, 0.9, 0.999, 1e-7, false),
+                userLoss = Loss.SparseCategoricalCrossentropy,
+                userMetrics = setOf("accuracy"),
+                userEpochs = 50,
+                userNewLayers = it.layers.mapIndexedTo(mutableSetOf()) { index, layer ->
+                    // Only train the last 3 layers
+                    if (it.layers.size - index <= 3) layer.layer.trainable()
+                    else layer.layer.trainable(false)
                 }
+            ).generateScript().shouldBeValid {
+                it.a shouldBe """
+                |import tensorflow as tf
+                |
+                |model = tf.keras.models.load_model("$modelName")
+                |
+                |newModel = tf.keras.Sequential([
+                |    model.get_layer("conv2d_6"),
+                |    model.get_layer("conv2d_7"),
+                |    model.get_layer("max_pooling2d_3"),
+                |    model.get_layer("dropout_6"),
+                |    model.get_layer("flatten_3"),
+                |    model.get_layer("dense_6"),
+                |    model.get_layer("dropout_7"),
+                |    model.get_layer("dense_7")
+                |])
+                |newModel.get_layer("conv2d_6").trainable = False
+                |newModel.get_layer("conv2d_7").trainable = False
+                |newModel.get_layer("max_pooling2d_3").trainable = False
+                |newModel.get_layer("dropout_6").trainable = False
+                |newModel.get_layer("flatten_3").trainable = False
+                |newModel.get_layer("dense_6").trainable = True
+                |newModel.get_layer("dropout_7").trainable = True
+                |newModel.get_layer("dense_7").trainable = True
+                |
+                |checkpointCallback = tf.keras.callbacks.ModelCheckpoint(
+                |    "sequential_3-weights.{epoch:02d}-{val_loss:.2f}.hdf5",
+                |    monitor="val_loss",
+                |    verbose=1,
+                |    save_best_only=False,
+                |    save_weights_only=True,
+                |    mode="auto",
+                |    save_freq="epoch",
+                |    load_weights_on_restart=False
+                |)
+                |
+                |newModel.compile(
+                |    optimizer=tf.keras.optimizers.Adam(0.001, 0.9, 0.999, 1.0E-7, False),
+                |    loss=tf.keras.losses.sparse_categorical_crossentropy,
+                |    metrics=["accuracy"]
+                |)
+                |
+                |earlyStoppingCallback = tf.keras.callbacks.EarlyStopping(
+                |    monitor="val_loss",
+                |    min_delta=0,
+                |    patience=10,
+                |    verbose=1,
+                |    mode="auto",
+                |    baseline=None,
+                |    restore_best_weights=False
+                |)
+                |
+                |(xTrain, yTrain), (xTest, yTest) = tf.keras.datasets.mnist.load_data()
+                |
+                |scaledXTest = xTest.reshape(-1, 28, 28, 1) / 255
+                |
+                |scaledXTrain = xTrain.reshape(-1, 28, 28, 1) / 255
+                |
+                |newModel.fit(
+                |    scaledXTrain,
+                |    yTrain,
+                |    batch_size=None,
+                |    epochs=50,
+                |    verbose=2,
+                |    callbacks=[checkpointCallback, earlyStoppingCallback],
+                |    validation_data=(scaledXTest, yTest),
+                |    shuffle=True
+                |)
+                """.trimMargin()
             }
         }
     }
---
+++ training/src/test/kotlin/edu/wpi/axon/training/TrainTestUtil.kt
@@ -0,0 +1,23 @@
+package edu.wpi.axon.training
+
+import edu.wpi.axon.tfdata.Model
+import edu.wpi.axon.tflayerloader.DefaultLayersToGraph
+import edu.wpi.axon.tflayerloader.LoadLayersFromHDF5
+import io.kotlintest.assertions.arrow.either.shouldBeRight
+import java.io.File
+
+/**
+ * Loads a model with name [modelName] from the test resources.
+ *
+ * @param modelName The name of the model.
+ * @param stub Used to get the calling class. Do not use this parameter.
+ * @return The model and its path.
+ */
+internal fun loadModel(modelName: String, stub: () -> Unit = {}): Pair<Model, String> {
+    val localModelPath = stub::class.java.getResource(modelName).toURI().path
+    val layers = LoadLayersFromHDF5(DefaultLayersToGraph())
+        .load(File(localModelPath))
+    val model = layers.attempt().unsafeRunSync()
+    model.shouldBeRight()
+    return model.b as Model to localModelPath
+}
