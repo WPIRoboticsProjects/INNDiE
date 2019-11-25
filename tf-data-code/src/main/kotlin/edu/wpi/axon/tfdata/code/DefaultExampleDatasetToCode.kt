fa0fb232d3b81044e00e40f468a92a2888b40a8b
Ryan Benasutti <firey45@gmail.com>
2019-11-24 22:47:27 -0500

    #119: Fix tests

--- tf-data-code/src/main/kotlin/edu/wpi/axon/tfdata/code/DefaultExampleDatasetToCode.kt
+++ tf-data-code/src/main/kotlin/edu/wpi/axon/tfdata/code/DefaultExampleDatasetToCode.kt
@@ -5,5 +5,5 @@ import edu.wpi.axon.tfdata.Dataset
 class DefaultExampleDatasetToCode : ExampleDatasetToCode {
 
     override fun datasetToCode(dataset: Dataset.ExampleDataset) =
-        "tf.keras.datasets.${dataset.name}.load_data()"
+        "tf.keras.datasets.${dataset.name}"
 }
