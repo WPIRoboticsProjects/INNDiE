8a6ab112796499a988a5a364b893fb6ae0a8bf5e
Austin Shalit <austinshalit@gmail.com>
2020-02-01 14:29:50 -0500

    Accept file instead of source & add controls property

--- ui-vaadin/src/main/kotlin/edu/wpi/axon/ui/component/Video.kt
+++ ui-vaadin/src/main/kotlin/edu/wpi/axon/ui/component/Video.kt
@@ -4,25 +4,43 @@ import com.github.mvysny.karibudsl.v10.VaadinDsl
 import com.github.mvysny.karibudsl.v10.init
 import com.vaadin.flow.component.Component
 import com.vaadin.flow.component.HasComponents
+import com.vaadin.flow.component.HasSize
+import com.vaadin.flow.component.HasStyle
 import com.vaadin.flow.component.Tag
 import com.vaadin.flow.dom.Element
+import com.vaadin.flow.server.InputStreamFactory
 import com.vaadin.flow.server.StreamResource
-import java.io.InputStream
+import java.io.File
+import java.io.FileInputStream
 
 @Tag("video")
-class Video(source: Source) : Component() {
-    class Source(name: String, factory: () -> InputStream, type: String = "video/mp4") : Element("source") {
+class Video(file: File) : Component(), HasSize, HasStyle {
+    private class Source(file: File) : Element("source") {
         init {
-            setAttribute("type", type)
-            setAttribute("src", StreamResource(name, factory))
+            setAttribute("src", StreamResource(file.name, InputStreamFactory { FileInputStream(file) }))
         }
     }
 
+    var controls: Boolean
+        get() = element.hasAttribute("controls")
+        set(value) {
+            if (value) {
+                element.setAttribute("controls", "")
+            } else {
+                element.removeAttribute("controls")
+            }
+        }
+
+    var volume: Double
+        get() = element.getProperty("volume", 0.0)
+        set(value) { element.setProperty("volume", value) }
+
     init {
-        element.appendChild(source)
-        element.setAttribute("controls", "")
+        controls = true
+
+        element.appendChild(Source(file))
     }
 }
 
 @VaadinDsl
-fun (@VaadinDsl HasComponents).video(source: Video.Source, block: (@VaadinDsl Video).() -> Unit = {}) = init(Video(source), block)
+fun (@VaadinDsl HasComponents).video(file: File, block: (@VaadinDsl Video).() -> Unit = {}) = init(Video(file), block)
