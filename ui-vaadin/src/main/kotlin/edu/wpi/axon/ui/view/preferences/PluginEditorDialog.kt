c60f426053ef224fc5450271de386b18c2baa59f
Austin Shalit <austinshalit@gmail.com>
2020-02-03 20:52:46 -0500

    Make dialog bigger

--- ui-vaadin/src/main/kotlin/edu/wpi/axon/ui/view/preferences/PluginEditorDialog.kt
+++ ui-vaadin/src/main/kotlin/edu/wpi/axon/ui/view/preferences/PluginEditorDialog.kt
@@ -1,19 +1,23 @@
 package edu.wpi.axon.ui.view.preferences
 
 import com.github.mvysny.karibudsl.v10.KComposite
+import com.github.mvysny.karibudsl.v10.alignSelf
 import com.github.mvysny.karibudsl.v10.beanValidationBinder
 import com.github.mvysny.karibudsl.v10.bind
 import com.github.mvysny.karibudsl.v10.button
 import com.github.mvysny.karibudsl.v10.dialog
+import com.github.mvysny.karibudsl.v10.horizontalAlignSelf
 import com.github.mvysny.karibudsl.v10.horizontalLayout
 import com.github.mvysny.karibudsl.v10.onLeftClick
 import com.github.mvysny.karibudsl.v10.setPrimary
 import com.github.mvysny.karibudsl.v10.textArea
 import com.github.mvysny.karibudsl.v10.textField
+import com.github.mvysny.karibudsl.v10.verticalAlignSelf
 import com.github.mvysny.karibudsl.v10.verticalLayout
 import com.vaadin.flow.component.dialog.Dialog
 import com.vaadin.flow.component.icon.Icon
 import com.vaadin.flow.component.icon.VaadinIcon
+import com.vaadin.flow.component.orderedlayout.FlexComponent
 import edu.wpi.axon.plugin.Plugin
 import edu.wpi.axon.plugin.PluginManager
 import edu.wpi.axon.ui.view.HasNotifications
@@ -27,19 +31,24 @@ class PluginEditorDialog(pluginManager: PluginManager, bean: Plugin? = null, onS
         ui {
             dialog {
                 dialog = this
+                width = "50vw"
                 verticalLayout {
                     textField("Name") {
+                        setWidthFull()
                         addValueChangeListener { binder.validate() }
                         bind(binder).asRequired()
                                 .withValidator({ name -> !pluginManager.listPlugins().map { it.name }.contains(name) || name == bean?.name }, "A plugin with that name already exists!")
                                 .bind(Plugin::name)
                     }
                     textArea("Content") {
+                        setWidthFull()
+                        height = "40vh"
                         addValueChangeListener { binder.validate() }
                         bind(binder).asRequired()
                                 .bind(Plugin::contents)
                     }
                     horizontalLayout {
+                        horizontalAlignSelf = FlexComponent.Alignment.END
                         button("Cancel") {
                             onLeftClick {
                                 this@dialog.close()
