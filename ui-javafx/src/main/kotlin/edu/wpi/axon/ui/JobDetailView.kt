ee93afa54d39194cf8b7f3bea5412804cef23cf4
Ryan Benasutti <firey45@gmail.com>
2020-02-08 15:01:32 -0500

    Add editor form tab pane

--- ui-javafx/src/main/kotlin/edu/wpi/axon/ui/JobDetailView.kt
+++ ui-javafx/src/main/kotlin/edu/wpi/axon/ui/JobDetailView.kt
@@ -11,6 +11,8 @@ import javafx.geometry.Pos
 import javafx.scene.control.Button
 import javafx.scene.control.ComboBox
 import javafx.scene.control.Label
+import javafx.scene.control.Tab
+import javafx.scene.control.TabPane
 import javafx.scene.layout.AnchorPane
 import javafx.scene.layout.HBox
 import javafx.scene.layout.VBox
@@ -34,58 +36,106 @@ class JobDetailView(
     private val jobInProgressProperty = SimpleBooleanProperty()
 
     init {
+        maxWidth = Double.MAX_VALUE
+        prefWidth = 300.0
+
         jobProperty.addListener { _, _, newValue ->
             jobInProgressProperty.value = (newValue.status != TrainingScriptProgress.NotStarted)
         }
 
         jobProperty.value = job
 
-        children.add(VBox().apply {
-            spacing = 10.0
-            padding = Insets(5.0)
+        children.add(TabPane().apply {
+            maxWidth = Double.MAX_VALUE
+            setTopAnchor(this, 0.0)
+            setRightAnchor(this, 0.0)
+            setBottomAnchor(this, 0.0)
+            setLeftAnchor(this, 0.0)
 
-            children.add(HBox().apply {
-                maxWidth = Double.MAX_VALUE
-                alignment = Pos.CENTER
-                children.add(Label(job.name).apply {
-                    font = Font.font(Font.getDefault().family, FontWeight.BOLD, 15.0)
-                })
-            })
+            tabs.add(Tab().apply {
+                isClosable = false
+                text = "Basic"
+                content = VBox().apply {
+                    spacing = 10.0
+                    padding = Insets(5.0)
 
-            children.add(ComboBox<Dataset>().apply {
-                disableProperty().bind(jobInProgressProperty)
-                items.setAll(Dataset.ExampleDataset::class.sealedSubclasses.mapNotNull {
-                    it.objectInstance
-                })
-                cellFactory = Callback { DatasetCell() }
-                buttonCell = DatasetCell()
-                selectionModel.select(job.userDataset)
-                selectionModel.selectedItemProperty().addListener { _, _, newValue ->
-                    // Update our copy of the job and push it to the db when the user selects a new
-                    // dataset
-                    scope.launch {
-                        jobProperty.value = jobDb.update(
-                            jobProperty.value.id,
-                            userDataset = newValue
-                        )
-                    }
+                    children.add(HBox().apply {
+                        maxWidth = Double.MAX_VALUE
+                        alignment = Pos.CENTER
+                        children.add(Label(job.name).apply {
+                            font = Font.font(Font.getDefault().family, FontWeight.BOLD, 15.0)
+                        })
+                    })
+
+                    children.add(Button("Run").apply {
+                        disableProperty().bind(jobInProgressProperty)
+                        setOnAction {
+                            scope.launch {
+                                jobProperty.value = jobDb.update(
+                                    jobProperty.value.id,
+                                    status = TrainingScriptProgress.Initializing
+                                )
+                            }
+                        }
+                    })
+
+                    children.add(Button("Close").apply {
+                        setOnAction { onClose() }
+                    })
                 }
             })
 
-            children.add(Button("Run").apply {
-                disableProperty().bind(jobInProgressProperty)
-                setOnAction {
-                    scope.launch {
-                        jobProperty.value = jobDb.update(
-                            jobProperty.value.id,
-                            status = TrainingScriptProgress.Initializing
-                        )
-                    }
+            tabs.add(Tab().apply {
+                isClosable = false
+                text = "Input"
+                content = VBox().apply {
+                    spacing = 10.0
+                    padding = Insets(5.0)
+
+                    children.add(HBox().apply {
+                        spacing = 5.0
+                        alignment = Pos.CENTER_LEFT
+
+                        children.add(Label("Dataset"))
+                        children.add(ComboBox<Dataset>().apply {
+                            disableProperty().bind(jobInProgressProperty)
+                            items.setAll(Dataset.ExampleDataset::class.sealedSubclasses.mapNotNull {
+                                it.objectInstance
+                            })
+                            cellFactory = Callback { DatasetCell() }
+                            buttonCell = DatasetCell()
+                            selectionModel.select(job.userDataset)
+                            selectionModel.selectedItemProperty().addListener { _, _, newValue ->
+                                // Update our copy of the job and push it to the db when the user selects a new
+                                // dataset
+                                scope.launch {
+                                    jobProperty.value = jobDb.update(
+                                        jobProperty.value.id,
+                                        userDataset = newValue
+                                    )
+                                }
+                            }
+                        })
+                    })
                 }
             })
 
-            children.add(Button("Close").apply {
-                setOnAction { onClose() }
+            tabs.add(Tab().apply {
+                isClosable = false
+                text = "Training"
+                content = VBox().apply {
+                    spacing = 10.0
+                    padding = Insets(5.0)
+                }
+            })
+
+            tabs.add(Tab().apply {
+                isClosable = false
+                text = "Output"
+                content = VBox().apply {
+                    spacing = 10.0
+                    padding = Insets(5.0)
+                }
             })
         })
     }
