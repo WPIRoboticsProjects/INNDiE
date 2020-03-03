f356a940852a464b1de06f26e92692199fc19a9d
Ryan Benasutti <firey45@gmail.com>
2020-03-03 14:24:43 -0500

    Clean up JobResultsView impl

--- ui-javafx/src/main/kotlin/edu/wpi/axon/ui/view/JobResultsView.kt
+++ ui-javafx/src/main/kotlin/edu/wpi/axon/ui/view/JobResultsView.kt
@@ -26,110 +26,104 @@ class JobResultsView : Fragment() {
     private val jobLifecycleManager by di<JobLifecycleManager>()
 
     override val root = borderpane {
-        centerProperty().bind(job.status.objectBinding { progress ->
-            when (progress) {
-                TrainingScriptProgress.Completed -> VBox().apply {
-                    spacing = 10.0
+        centerProperty().bind(job.itemProperty.objectBinding(job.status) { jobDto ->
+            if (jobDto == null) {
+                Label("No selection.")
+            } else {
+                when (val status = jobDto.status) {
+                    TrainingScriptProgress.Completed -> VBox().apply {
+                        spacing = 10.0
 
-                    // TODO: Race condition accessing job.id. Sometimes null, sometimes not.
-                    LOGGER.debug {
-                        "Loading results for Job id: ${job.id.value}"
-                    }
+                        LOGGER.debug {
+                            "Loading results for Job id: ${jobDto.id}"
+                        }
 
-                    val jobResults = job.id.objectBinding {
-                        if (it == null) {
-                            emptyList()
-                        } else {
-                            jobLifecycleManager.listResults(it.toInt())
-                        }.toObservable()
-                    }
+                        val jobResults = jobLifecycleManager.listResults(jobDto.id).toObservable()
 
-                    LOGGER.debug {
-                        "Job results:\n${jobResults.value?.joinToString("\n")}"
-                    }
+                        LOGGER.debug {
+                            "Job results:\n${jobResults.joinToString("\n")}"
+                        }
 
-                    val id = job.id.value?.toInt()
-                    val trainingLogResult =
-                        jobResults.value!!.firstOrNull { it == "trainingLog.csv" }
+                        val trainingLogResult = jobResults.firstOrNull { it == "trainingLog.csv" }
 
-                    if (id != null && trainingLogResult != null) {
-                        linechart(
-                            "Training Results",
-                            x = NumberAxis(),
-                            y = NumberAxis()
-                        ) {
-                            xAxis.label = "Epoch"
-
-                            val trainingLogData =
-                                jobLifecycleManager.getResult(id, trainingLogResult).readLines()
-                            val colNames = trainingLogData.first().split(',')
-
-                            multiseries(
-                                names = *colNames.mapNotNull {
-                                    if (it == "epoch") null else it
-                                }.toTypedArray()
+                        if (trainingLogResult != null) {
+                            linechart(
+                                "Training Results",
+                                x = NumberAxis(),
+                                y = NumberAxis()
                             ) {
-                                val epochIndex = colNames.indexOf("epoch")
-                                trainingLogData.drop(1).forEach {
-                                    val values = it.split(',')
-                                    data(
-                                        values[epochIndex].toInt(),
-                                        *values.mapIndexedNotNull { index, data ->
-                                            if (index == epochIndex) null
-                                            else data.toDouble()
-                                        }.toTypedArray()
-                                    )
+                                xAxis.label = "Epoch"
+
+                                val trainingLogData = jobLifecycleManager.getResult(jobDto.id, trainingLogResult).readLines()
+                                val colNames = trainingLogData.first().split(',')
+
+                                multiseries(
+                                    names = *colNames.mapNotNull {
+                                        if (it == "epoch") null else it
+                                    }.toTypedArray()
+                                ) {
+                                    val epochIndex = colNames.indexOf("epoch")
+                                    trainingLogData.drop(1).forEach {
+                                        val values = it.split(',')
+                                        data(
+                                            values[epochIndex].toInt(),
+                                            *values.mapIndexedNotNull { index, data ->
+                                                if (index == epochIndex) null
+                                                else data.toDouble()
+                                            }.toTypedArray()
+                                        )
+                                    }
                                 }
                             }
                         }
-                    }
 
-                    listview<String> {
-                        itemsProperty().bind(jobResults)
-                        selectionModel.selectionMode = SelectionMode.SINGLE
-                        isEditable = false
-                        selectionModel.selectedItemProperty().addListener { _, _, newValue ->
-                            if (newValue == null) {
-                                bottom = null
-                            } else {
-                                bottom {
-                                    add(
-                                        find<ResultFragment>(
-                                            mapOf(
-                                                ResultFragment::data to LazyResult(
-                                                    newValue,
-                                                    lazy {
-                                                        jobLifecycleManager.getResult(
-                                                            job.id.value.toInt(),
-                                                            newValue
-                                                        )
-                                                    }
+                        listview<String> {
+                            items = jobResults
+                            selectionModel.selectionMode = SelectionMode.SINGLE
+                            isEditable = false
+                            selectionModel.selectedItemProperty().addListener { _, _, newValue ->
+                                if (newValue == null) {
+                                    bottom = null
+                                } else {
+                                    bottom {
+                                        add(
+                                            find<ResultFragment>(
+                                                mapOf(
+                                                    ResultFragment::data to LazyResult(
+                                                        newValue,
+                                                        lazy {
+                                                            jobLifecycleManager.getResult(
+                                                                job.id.value.toInt(),
+                                                                newValue
+                                                            )
+                                                        }
+                                                    )
                                                 )
                                             )
                                         )
-                                    )
+                                    }
                                 }
                             }
                         }
                     }
-                }
 
-                is TrainingScriptProgress.Error -> VBox().apply {
-                    spacing = 10.0
-                    label("Job completed erroneously. Error Log:")
-                    textarea {
-                        text = progress.log
-                        isEditable = false
-                        isWrapText = true
+                    is TrainingScriptProgress.Error -> VBox().apply {
+                        spacing = 10.0
+                        label("Job completed erroneously. Error Log:")
+                        textarea {
+                            text = status.log
+                            isEditable = false
+                            isWrapText = true
+                        }
                     }
-                }
 
-                TrainingScriptProgress.NotStarted -> Label("Job has not been started yet.")
-                TrainingScriptProgress.Creating, TrainingScriptProgress.Initializing -> Label("Job is starting.")
-                is TrainingScriptProgress.InProgress -> Label("Job has not finished training yet.")
-                null -> Label("No Job selected.")
-            }.apply {
-                paddingAll = 10
+                    TrainingScriptProgress.NotStarted -> Label("Job has not been started yet.")
+                    TrainingScriptProgress.Creating, TrainingScriptProgress.Initializing -> Label("Job is starting.")
+                    is TrainingScriptProgress.InProgress -> Label("Job has not finished training yet.")
+                    null -> Label("No Job selected.")
+                }.apply {
+                    paddingAll = 10
+                }
             }
         })
     }
