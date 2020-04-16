c247703e71a9c55edbc15c30be3ef16ecf78a283
Octogonapus <firey45@gmail.com>
2020-04-16 19:53:31 -0400

    Cleanup

--- plugin/src/main/kotlin/edu/wpi/axon/plugin/ProcessTestOutputPlugins.kt
+++ plugin/src/main/kotlin/edu/wpi/axon/plugin/ProcessTestOutputPlugins.kt
@@ -7,13 +7,13 @@ object ProcessTestOutputPlugins {
         """
         |def process_model_output(model_input, expected_output, model_output):
         |    import numpy as np
-        |    
+        |
         |    with open("output/expected_output.txt", "w+") as f:
         |        np.savetxt(f, expected_output)
-        |    
+        |
         |    with open("output/model_output.txt", "w+") as f:
         |        np.savetxt(f, model_output)
-        |    
+        |
         |    images = tf.image.encode_jpeg(tf.cast(model_input[0]*255, tf.uint8))
         |    fwrite = tf.write_file(
         |        tf.constant("output/model_input.jpeg"),
